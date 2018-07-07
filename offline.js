8ab8c4bd7a69d06258a9a81ab7780b5b295c0ea1
Ivan3424 <wwj3424v@163.com>
2018-07-07 11:49:31 +0800

    first

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1530935304,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/tiledbackground.png",
+		"images/block-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/darkblock-sheet0.png",
+		"images/touchobj-sheet0.png",
+		"images/line-sheet0.png",
+		"images/water-sheet0.png",
+		"images/number.png",
+		"images/bg_start.png",
+		"images/bg_hint.png",
+		"images/bg_flower.png",
+		"images/bg_foot.png",
+		"images/step.png",
+		"images/bg_end.png",
+		"images/number2.png",
+		"images/bg_back.png",
+		"images/particles.png",
+		"images/combo.png",
+		"images/tiledbackground2.png",
+		"images/spritefont.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/bg_hint2.png"
+	]
+}
